fa7b97d1ead75351975764be88f439cbe722c607
alexcactus0 <217364473+alexcactus0@users.noreply.github.com>
2026-02-17 22:45:00 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  globals: true,
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+  },
+});
